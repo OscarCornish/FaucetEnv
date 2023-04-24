a54394c1f92874677ca2c17adf1f987fca2fcb25
Oscar Cornish <oscar.cornish@outlook.com>
2023-04-24 17:45:07 +0100

    Working TCP_ACK_Bounce

--- namespaces.jl
+++ namespaces.jl
@@ -78,11 +78,11 @@ get_devs(netns::String="")::String = read(cmdify("$(ns(netns))ip a"), String)
 # All have FAUCET_PREFIX prepended to them
 
 
+NAMESPACES = ["bridge", "sender", "receiver", "aux"]
 
 function create_Faucet_env()::Nothing
-    namespaces = ["bridge", "sender", "receiver", "aux"]
-    @info "Created namespaces." namespaces
-    create_namespace.(namespaces)
+    @info "Created namespaces." NAMESPACES
+    create_namespace.(NAMESPACES)
     @info "Faucet namespaces" get_faucet_namespaces()
 
     # Create sender veth pairs
@@ -97,9 +97,9 @@ function create_Faucet_env()::Nothing
     move_dev("recvb", "bridge", "receiver")
     set_dev_address("recva", "10.20.30.2/24", "receiver")
 
-    # Create veth pair for aux
+    # Create aux veth pairs
     create_veth_pair("auxa", "auxb", "aux")
-    # Move auxb to bridge namespace
+    # Move auxb to receiver namespace
     move_dev("auxb", "bridge", "aux")
     set_dev_address("auxa", "10.20.30.201/24", "aux")
 
@@ -114,24 +114,26 @@ function create_Faucet_env()::Nothing
     bridge_devs = get_devs("bridge")
     sender_devs = get_devs("sender")
     receiver_devs = get_devs("receiver")
-    aux_devs = get_devs("aux")
 
     # Bring devices up
     set_dev_up("senda", "sender")
-    set_dev_up("recva", "receiver")
-    set_dev_up("auxa", "aux")
     set_dev_up("sendb", "bridge")
+
+    set_dev_up("recva", "receiver")
     set_dev_up("recvb", "bridge")
+    
+    set_dev_up("auxa", "aux")
     set_dev_up("auxb", "bridge")
+
     set_dev_up("br0", "bridge")
 
-    @debug "Devs created and setup" bridge_devs sender_devs receiver_devs aux_devs
+    @debug "Devs created and setup" bridge_devs sender_devs receiver_devs 
     return nothing
 end
 
 function teardown()::Nothing
-    namespaces = ["bridge", "sender", "receiver", "aux"]
-    @info "Deleting namespaces." namespaces
-    delete_namespace.(namespaces)
+    @info "Deleting namespaces." NAMESPACES
+    delete_namespace.(NAMESPACES)
     @info "Faucet namespaces" get_faucet_namespaces()
+    return nothing
 end
